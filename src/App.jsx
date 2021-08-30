27256e636a15f012e31d62664a423f99b1c9f6cf
anarativa09 <anarativa09@gmail.com>
2021-08-30 08:13:36 -0500

    Refactor: Add stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
 
 // Import views
 import Home from './views/Home';
 import NotFound from './views/NotFound';
+
+// Import styles
+import './styles/index.scss';
 
 function App() {
   return (
