d1e6224efef95d68d4465a82ba312865d5b8cdae
anarativa09 <anarativa09@gmail.com>
2021-08-30 22:26:25 -0500

    Feat: Add collection view

---
+++ src/views/Collection.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+// Import components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import StarRatingView from '../components/StarRatingView';
+
+function Collection() {
+  const localData = JSON.parse(sessionStorage.getItem('comics'));
+
+  if (localData === null || localData.length === 0) {
+    return (
+      <>
+        <Header />
+        <p> No has calificado ningún comic todavía. 🗯️💥</p>
+        <Footer />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+
+      <section className="comics-ranked-container">
+        { localData.map((comic) => (
+          <Card className="text-center card-container">
+            <Card.Header>
+              <div className="flex-row">
+                {`#${comic.dataComic.num}`}
+                <StarRatingView data={comic} />
+              </div>
+            </Card.Header>
+
+            <Card.Body>
+              <Card.Title>{comic.dataComic.title}</Card.Title>
+              <Card.Img src={comic.dataComic.img} className="comic-img" alt={comic.dataComic.title} />
+            </Card.Body>
+          </Card>
+        ))}
+      </section>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Collection;
